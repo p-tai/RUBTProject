760680c23afcfdbcc9ec37a678d877904b30c2a7
zhangoose <az177@kill.cs.rutgers.edu>
2014-03-02 16:24:12 -0500

    gracefully exited dat shit

--- RUBTClient.java
+++ RUBTClient.java
@@ -18,27 +18,60 @@ import java.lang.*;
 
 /**
  * For le error checking
+ * 
  */
 public class RUBTClient {
-	public static void main(String[] args){
-    	String filePath = "project1.torrent";
-        String picture = "picture.jpg";
-        
-        Client client = new Client(filePath, picture);
-        client.HTTPGET();
-        client.printPeerList();
-        String[] getPeerList = client.getPeerList();
-        String peer = "";
-        if(getPeerList != null){
-        	for(int i = 0; i < getPeerList.length; i++){
-        		if(getPeerList[i].contains("RUBT11")){
-        			peer = getPeerList[i];
-        		}
-        	}
-        }
-        //TODO FIX THIS!
-        client.connect(peer);
-        return;
-	}
-	
-}
+	public static void main(String[] args) throws InterruptedException{
+    	
+    		/*
+		 * "how to gracefully handle sigkill"
+		 * http://stackoverflow.com/questions/2541597/how-to-gracefully-handle-the-sigkill-signal-in-java
+		 */
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			/*
+			 * for the teddy bear: 
+			 * http://www.chris.com/ascii/index.php?art=animals/bears+(teddybears)
+			 * for the music notes:
+			 * http://1lineart.kulaone.com/
+			 */
+			public void run(){
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println("      ('-^-/')     /-----------------\\");
+				System.out.println("      `o__o' ]     |    byebye <3    |");
+				System.out.println("      (_Y_) _/     |  ♬·¯·♩¸¸♪·¯·♫ ♪ |");
+				System.out.println("    _..`--'-.`,	  /,_________________| ");
+				System.out.println("   (__)_,--(__)    ");
+				System.out.println("       7:   ; 1");
+				System.out.println("     _/,`-.-' :");
+				System.out.println("    (_,)-~~(_,)");
+
+			}//end of run
+		});//end of new thread runtime thingie :3
+		while(true){
+			//purely for testing purposes, get rid of thread later
+			Thread.sleep(1000);
+
+			//start of yo shitz
+			String filePath = "project1.torrent";
+			String picture = "picture.jpg";
+			
+			Client client = new Client(filePath, picture);
+			client.HTTPGET();
+			client.printPeerList();
+			String[] getPeerList = client.getPeerList();
+			String peer = "";
+			if(getPeerList != null){
+				for(int i = 0; i < getPeerList.length; i++){
+					if(getPeerList[i].contains("RUBT11")){
+						peer = getPeerList[i];
+  					}//end of if 
+				}//end of for 
+			}//end of if 
+            //TODO FIX THIS!
+			client.connect(peer);
+
+		}//end ofw hile 
+	}//end of public static void main
+}//end of class
